1f176447c10f7f829730033bbc9b1fdaae94daa8
n1c0x <nicolas.renard67@gmail.com>
2015-02-21 17:20:33 +0100

    Création du bundle

---
+++ src/omg/gotBundle/Controller/DefaultController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace omg\gotBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return new Response('<html><body>Hello '.$name.'</body></html>');
+        #return $this->render('omggotBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
