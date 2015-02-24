e136e4dd185e86f692e4be47f4b4a90a3baa04de
n1c0x <nicolas.renard67@gmail.com>
2015-02-25 00:16:23 +0100

    Ajout de la fonction d'ajout de personnage. Non terminé

--- src/omg/gotBundle/Controller/GotController.php
+++ src/omg/gotBundle/Controller/GotController.php
@@ -16,13 +16,47 @@ class GotController extends Controller
 		$name = "accueil";
         return $this->render('omggotBundle:Got:accueil.html.twig', array('name' => $name));
 	}
-	public function ajouterAction()
+	public function ajouterAction(Request $request)
 	{
+		# Création d'un nouvel objet personnage
+		$personnage = new Personnage();
+
+		# Ajout des champs
+		$form = $this->createFormBuilder($personnage)
+			->add('nom','text')
+			->add('description','text')
+			#->add('maison','text')
+			#->add('age','text')
+			//->add('competences','text')
+			->add('save','submit')
+			->getForm();
 		
+		$form->handleRequest($request);
+
+		if ($form->isValid()){
+			//$em = $this->getDoctrine()->getManager();
+			//$repository_personnage = $em->getRepository('omggotBundle:Got');
+			# On insère l'instance de personnage dans la liste des élements à enregistrer en base.
+			# Les données ne sont pas encore écrites
+			//$em->persist($personnage);
+
+			# On confirme l'exécution de la requète
+			//$em->flush;
+
+			return $this->redirect($this->generateUrl('got_success'));
+		}
+
+		# Génération du formulaire
+		return $this->render('omggotBundle:Got:ajouter.html.twig', array('form' => $form->createView()));
 	}
     public function rechercherAction()
     {
 		$name = "rechercher";
         return $this->render('omggotBundle:Got:rechercher.html.twig', array('name' => $name));
+    }
+    public function successAction()
+    {
+		$name = "succes";
+        return $this->render('omggotBundle:Got:success.html.twig', array('name' => $name));
     }
 }
